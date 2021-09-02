a0cbf629f9d6d3acf62f25702afbdadcee06026e
Carmo-sousa <carmosousa20@outlook.com>
2021-09-02 06:51:19 -0400

    Módulo: Variáveis
    Aula: Desafio variáveis.

--- index.php
+++ index.php
@@ -46,6 +46,7 @@
                         <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis variáveis</a></li>
+                        <li><a href="exercicio.php?dir=variaveis&file=desafio_variaveis">Desafio variáveis</a></li>
                     </ul>
                 </div>
                 <div class="modulo indigo">
---
+++ variaveis/desafio_variaveis.php
@@ -0,0 +1,12 @@
+<h1 class="titulo">Desafio variáveis</h1>
+
+<?php
+$a = "Nossa";
+$Nossa = "Eu";
+$Eu = "consegui";
+$consegui = "responder";
+$responder = "esse";
+$esse = "desafio";
+
+echo "$a! {$$a} {$$$a} {$$$$a} {$$$$$a} {$$$$$$a}.";
+?>
