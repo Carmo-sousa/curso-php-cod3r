b8fcc92ea998fc4a056e5f190b8df81806fc5569
Carmo-sousa <carmosousa20@outlook.com>
2021-09-02 21:34:37 -0400

    Módulo: Variáveis
    Aula: Constantes.

--- index.php
+++ index.php
@@ -48,6 +48,7 @@
                         <li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis variáveis</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=desafio_variaveis">Desafio variáveis</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=valor_referencia">Valor vs Referência</a></li>
+                        <li><a href="exercicio.php?dir=variaveis&file=constantes">Constantes</a></li>
                     </ul>
                 </div>
                 <div class="modulo indigo">
---
+++ variaveis/constantes.php
@@ -0,0 +1,10 @@
+<h1 class="titulo">Constantes</h1>
+
+<?php
+define("PI", 3.1415);
+
+const MAX_RANGE = 100;
+
+echo '<br/>' . PI . '<br/>';
+echo MAX_RANGE;
+?>
