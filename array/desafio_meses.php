ad00b32b6995e34bbad74535da618e9951f70698
Carmo-sousa <carmosousa20@outlook.com>
2021-09-20 06:57:21 -0400

    Módulo: Array
    Aula: Desafio Meses

---
+++ array/desafio_meses.php
@@ -0,0 +1,20 @@
+<h1 class="title">Desafio Meses</h1><hr>
+
+<?php
+$meses = array(
+    1 => "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+);
+
+echo "$meses[12]";
+?>
