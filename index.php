5dc49f9221af7f0b64b8e262ffd4930342dee281
Carmo-sousa <carmosousa20@outlook.com>
2021-09-20 06:56:14 -0400

    Módulo: Array
    Aula: Desafio meses

--- index.php
+++ index.php
@@ -70,6 +70,7 @@
                     <li><a href="exercicio.php?dir=array&file=basico">Básico</a></li>
                     <li><a href="exercicio.php?dir=array&file=mapa">Mapa</a></li>
                     <li><a href="exercicio.php?dir=array&file=desafio_index">Desafio index</a></li>
+                    <li><a href="exercicio.php?dir=array&file=desafio_meses">Desafio Meses</a></li>
                     </ul>
                 </div>
                 <div class="modulo sea">
